b88399958b44e5096f54e8ed10e9395cf07d6882
Bruno Silva <brunoschdev25@gmail.com>
2023-06-30 14:01:06 -0300

    First commit

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               
+        }
+    }
+}
