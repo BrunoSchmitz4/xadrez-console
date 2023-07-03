a90b790f5b3bcbb273017f306431ffb6559921f1
Bruno Silva <brunosch250@gmail.com>
2023-07-03 14:59:58 -0300

    Update Rainha.cs

--- xadrez-console/xadrez/Rainha.cs
+++ xadrez-console/xadrez/Rainha.cs
@@ -7,15 +7,76 @@ using tabuleiro;
 
 namespace xadrez
 {
-    //class Rainha : Peca
-    //{
-    //    public Rainha(Tabuleiro tab, Cor cor) : base(tab, cor)
-    //    {
+    class Rainha : Peca
+    {
+        public Rainha(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
 
-    //    }
-    //    public override string ToString()
-    //    {
-    //        return "Q";
-    //    }
-    //}
+        }
+        public override string ToString()
+        {
+            return "Q";
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            // Esquerda
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+            while (tab.posicaoValida(pos) && podeMover(pos)){
+                mat[pos.linha, pos.coluna] = true;
+                if(tab.peca(pos) != null && tab.peca(pos).cor != cor){
+                    break;
+                }
+                pos.definirValores(pos.linha, pos.coluna - 1);
+            }
+
+            // Direita
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.linha, pos.coluna + 1);
+            }
+
+            // Acima
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.linha - 1, pos.coluna);
+            }
+
+            // Abaixo
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.linha + 1, pos.coluna);
+            }
+
+            // NO (continuar a partir da linha 64
+        }
+    }
 }
