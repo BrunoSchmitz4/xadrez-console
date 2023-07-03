9f036a4af07bbbc33c78e2efe1a6073d6c5e4a5c
Bruno Silva <brunosch250@gmail.com>
2023-07-03 08:48:14 -0300

    Aprimorando impressão e estilização de pecas e tabuleiros

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using tabuleiro;
 using xadrez;
-using xadrez_console.xadrez;
-
 namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            PosicaoXadrex pos = new PosicaoXadrex('a', 1);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+
+                Tela.imprimirTabuleiro(tab);
+            }catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            //Console.ReadLine();
         }
     }
 }
