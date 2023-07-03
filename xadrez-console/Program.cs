1fcaadd388eb8e0fb2706bb9a9be7cf337875029
Bruno Silva <brunosch250@gmail.com>
2023-07-03 08:29:17 -0300

    Criando PosicaoXadrex

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,21 +1,17 @@
 ﻿using System;
 using tabuleiro;
 using xadrez;
+using xadrez_console.xadrez;
+
 namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrex pos = new PosicaoXadrex('a', 1);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
-            Tela.imprimirTabuleiro(tab);
-            
-            Console.ReadLine();
+            Console.WriteLine(pos);
         }
     }
 }
---
+++ xadrez-console/xadrez/PosicaoXadrex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace xadrez_console.xadrez
+{
+    class PosicaoXadrex
+    {
+        public char coluna { get; set; }
+        public int linha { get; set;}
+
+        public PosicaoXadrex (char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
